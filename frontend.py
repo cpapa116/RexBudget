7327f74dec0f0b730ff2de9acb22b01a062da74e
Christian <cpapa@host-234-222.nyam2091.amherst.ny.us.clients.pavlovmedia.net>
2024-01-31 17:37:18 -0500

    basic labels added to frontend

--- frontend.py
+++ frontend.py
@@ -1,1 +1,29 @@
 # this will be for the frontend
+from tkinter import *
+from tkinter import ttk
+
+class Front:
+
+    def __init__():
+        # create main window
+        root = Tk()
+        root.minsize(640,480)
+        root.maxsize(1920,1080)
+
+        # create labels
+        accLabel = Label(root, text="Accounts")
+        amtLabel = Label(root, text="Amount")
+        cbLabel = Label(root, text="Cash Back")
+
+        # position labels
+        accLabel.grid(row=0, column=0)
+        amtLabel.grid(row=0, column=20)
+        cbLabel.grid(row=0, column=40)
+
+
+        # keeps main frame running
+        root.mainloop()
+
+
+if __name__ == '__main__':
+    Front.__init__()
