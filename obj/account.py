fb168e15506bc3e892a70ba10b001ffe53356edf
Jack <jack.chin520@gmail.com>
2024-01-31 12:40:49 -0500

    added set features to account class

--- obj/account.py
+++ obj/account.py
@@ -2,4 +2,18 @@ class Account():
     def __init__(self, name, money):
         self.name = name 
         self.money = money
-        pass
+    
+    def getName(self):
+        return self.name
+    
+    #* POST request
+    def setName(self, name):
+        self.name = name
+    
+    def getMoney(self):
+        return self.money  
+
+    #* POST request
+    def changeMoney(self, money):
+        self.money += money
+        return self.money  
